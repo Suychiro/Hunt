84315461d60a89e7701cbecceee18589b1837ab0
Daniel Dornhof <Daniel.Dornhof@stud.fh-luebeck.de>
2018-05-31 16:11:28 +0200

    Zwischenstand

--- WebTech/lib/src/control.dart
+++ WebTech/lib/src/control.dart
@@ -1,28 +1,112 @@
 part of hunt;
-int firstY;
-int lastY;
+
 
 
 class GameController{
 
+  var game = new Game();
+  var view = new GameView();
+
+  /**
+   * Periodic trigger spawning entities
+   */
+  Timer spawnTrigger;
+
+  /**
+   * Periodic trigger moving entities
+   */
+  Timer entityTrigger;
+
+  /**
+   * Periodic Trigger moving bullets
+   */
+
+  Timer bulletTrigger;
 
   GameController() {
-    //Character Movement by comparing first and last Y-value (Up/Down)
+    int firstY;
+    int lastY;
+    bool touchMoved = false;
+    /**
+     *  Character Movement by comparing first and last Y-value (Up/Down)
+     */
     window.onTouchStart.listen((TouchEvent e){                            //Determine first Y-value
+      firstY = 0;
       firstY = e.touches.first.client.y.toInt();
     });
 
     window.onTouchMove.listen((TouchEvent e) {                            //Determine last Y-value
       lastY = e.touches.last.client.y.toInt();
+      touchMoved = true;
     });
 
-    window.onTouchEnd.listen((TouchEvent e) {                             //Compares both Y-values
-      if (firstY < lastY && (lastY - firstY) > 70) {        //Swipe Down
-        querySelector('#output').text = "Down";
+  window.onTouchEnd.listen((TouchEvent e) {    //Compares both Y-values
+    if(game.running == true && touchMoved) {
+      if (firstY < lastY && (lastY - firstY) > 70) { //Swipe Down
+        game.character.moveDown();
       }
-      else if (firstY > lastY && (firstY - lastY) > 70) {   //Swipe Up
-        querySelector('#output').text = "Up";
+      else if (firstY > lastY && (firstY - lastY) > 70) { //Swipe Up
+        game.character.moveUp();
       }
+      firstY = 0;
+      lastY = 0;
+      touchMoved = false;
+      view.update(game);
+    }
+  });
+
+    /**
+     * Speed of Trigger
+     */
+
+
+   /*
+    spawnTrigger = new Timer.periodic(new Duration(seconds: 2),(_) => spawnEntities());
+    entityTrigger = new Timer.periodic(new Duration(milliseconds: 25),(_) => moveEntities());
+    bulletTrigger = new Timer.periodic(new Duration(milliseconds: 25), (_) => moveBullets());
+*/
+    view.startButton.onClick.listen((_) {
+      start();
+      spawnEntities();
+      querySelector("#menu").hidden = true;
+    });
+
+    view.shootButton.onClick.listen((_) {
+      game.shootBullet();
     });
   }
+
+
+
+
+
+  void start(){
+    view.createField(game);
+    game.running = true;
+  }
+
+  /**
+   * Makes the game spawn entities when triggered
+   */
+  void spawnEntities(){
+    game.spawnEntities();
+    view.update(game);
+  }
+
+  /**
+   * Makes the entities move when triggered
+   */
+  void moveEntities(){
+    game.moveEntities(50);
+    view.update(game);
+  }
+
+  /**
+   * Makes the bullets move when triggered
+   */
+  void moveBullets(){
+    game.moveBullets(50);
+    view.update(game);
+  }
+
 }
--- WebTech/lib/src/model.dart
+++ WebTech/lib/src/model.dart
@@ -1,1 +1,202 @@
 part of hunt;
+
+
+class Game{
+  int rows;
+  int level;
+  bool running;
+  bool gameOver;
+  int score;
+  Character character;
+  List<PickUp> pickups = new List<PickUp>();
+  List<Entity> entities = new List<Entity>();
+  List<Bullet> bullets = new List<Bullet>();
+
+ Game(){
+   this.rows = 3;
+   this.level = 1;
+   this.running = false;
+   this.gameOver = false;
+   this.score = 0;
+   this.character = new Character.On(this);
+ }
+
+
+  void moveEntities(int maxPos){
+    entities.forEach((x) => x.move());
+    for (int i = 0; i < entities.length - 1; i++)
+    {
+      if(entities.elementAt(i).currentPos <= 0){
+       if(entities.elementAt(i).row == character.currentRow){
+         entities.elementAt(i).onTouch();
+       }
+        entities.removeAt(i);
+      }
+      for(int j = 0; j< bullets.length; j++){
+        if(entities.elementAt(i).currentPos == bullets.elementAt(j).currentPos && entities.elementAt(i).row == bullets.elementAt(j).row){
+          entities.elementAt(i).onHit();
+          bullets.removeAt(j);
+        }
+      }
+
+    }
+  }
+
+  void movePickUps(){}
+
+  void moveBullets(int maxPos){
+   bullets.forEach((x) => x.move());
+    for (int i = 0; i < bullets.length - 1; i++)
+    {
+      if(bullets.elementAt(i).currentPos > 50){
+        entities.removeAt(i);
+      }
+      for(int j = 0; j < entities.length;j++){
+        if(entities.elementAt(i).currentPos == bullets.elementAt(j).currentPos && entities.elementAt(i).row == bullets.elementAt(j).row){
+          entities.elementAt(i).onHit();
+          bullets.removeAt(j);
+        }
+      }
+    }
+  }
+
+  void levelUp(){}
+
+  void spawnEntities() {
+   for(int row = 0; row <= rows; row++) {
+     int id = entities.length;
+
+     entities.add(new Enemy1.on(this, id, row));
+   }
+  }
+
+  void shootBullet(){
+   int id = bullets.length;
+   bullets.add(new Arrow(this,id,character.currentRow));
+  }
+
+  void addPoints(int points){
+    score = score + points;
+  }
+
+}
+///////////////////////////////////////////////////////////////////////
+class Character{
+  int currentRow = 1;
+  int ammo = 999;
+  double health = 3.0;
+  bool alive;
+  bool netOut;
+  bool noAmmo;
+  Game _game;
+
+  void shootBullet(){
+    if(!noAmmo) {
+      ammo = ammo - 1;
+      if(ammo == 0){
+        noAmmo = true;
+      }
+    }
+  }
+
+  void shootNet(){}
+
+  void moveUp(){
+    if(currentRow > 0) {
+      currentRow = currentRow - 1;
+    }
+  }
+
+  void moveDown(){
+    if(currentRow < _game.rows) {
+      currentRow = currentRow + 1;
+    }
+  }
+
+  void receiveDamage(double damage){
+    health = health - damage;
+  }
+
+  Character.On(Game _game){
+    this._game = _game;
+    alive = true;
+    netOut = false;
+    noAmmo = false;
+  }
+}
+//////////////////////////////////////////////////////////////////////
+abstract class Entity{
+  int id;
+  int row;
+  int currentPos;
+  int health;
+  int speed;
+  int lootProbability;
+  int points;
+  double damage;
+  bool alive;
+  void onHit();
+  void onDeath();
+  void onCatch();
+  void onTouch();
+  void move();
+  Game _game;
+}
+
+class Enemy1 extends Entity{
+  final health = 1;
+  final points = 1;
+  final damage = 1.0;
+  final speed = 1;
+
+  void onHit(){
+    health = health - 1;
+  }
+
+  void onDeath(){
+    _game.entities.remove(this);
+    _game.addPoints(points);
+  }
+
+  void onCatch(){}
+
+  void onTouch(){
+    _game.character.receiveDamage(damage);
+  }
+
+  void move(){
+    currentPos = currentPos - speed;
+  }
+
+  Enemy1.on(Game _game, int id,int row){
+    this._game = _game;
+    this.id = id;
+    this.row = row;
+  }
+}
+
+/////////////////////////////////////////////////////////////////////
+abstract class Bullet{
+  int id;
+  int row;
+  int currentPos;
+  void move();
+  Game _game;
+}
+
+class Arrow extends Bullet{
+
+  Arrow(Game_game,id,row){
+    this._game = _game;
+    this.id = id;
+    this.row = row;
+  }
+
+  void move(){
+    currentPos = currentPos + 1;
+  }
+}
+//////////////////////////////////////////////////////////////////////
+abstract class PickUp{
+
+}
